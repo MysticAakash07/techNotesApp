1256cd6cdf705df37506f2da7e025c7c5be71595
MysticAakash07 <rahulsagar460@gmail.com>
2025-08-01 17:02:22 +0530

    feat(auth): Wrapped the auth route using PresistLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PresistLogin";
 
 function App() {
 	return (
@@ -18,27 +19,31 @@ function App() {
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Public />} />
 				<Route path="login" element={<Login />} />
-				<Route element={<Prefetch />}>
-					<Route path="dash" element={<DashLayout />}>
-						<Route index element={<Welcome />} />
 
-						<Route path="users">
-							<Route index element={<UsersList />} />
-							<Route path=":id" element={<EditUser />} />
-							<Route path="new" element={<NewUserForm />} />
-						</Route>
-						{/* End Users */}
+				<Route element={<PersistLogin />}>
+					<Route element={<Prefetch />}>
+						<Route path="dash" element={<DashLayout />}>
+							<Route index element={<Welcome />} />
 
-						<Route path="notes">
-							<Route index element={<NotesList />} />
-							<Route path=":id" element={<EditNote />} />
-							<Route path="new" element={<NewNote />} />
+							<Route path="users">
+								<Route index element={<UsersList />} />
+								<Route path=":id" element={<EditUser />} />
+								<Route path="new" element={<NewUserForm />} />
+							</Route>
+							{/* End Users */}
+
+							<Route path="notes">
+								<Route index element={<NotesList />} />
+								<Route path=":id" element={<EditNote />} />
+								<Route path="new" element={<NewNote />} />
+							</Route>
+							{/* End Notes */}
 						</Route>
-						{/* End Notes */}
+						{/* End Dash */}
 					</Route>
-					{/* End Dash */}
+					{/* End Prefetch */}
 				</Route>
-				{/* End Prefetch */}
+				{/* End PresistLogin */}
 			</Route>
 		</Routes>
 	);

aa777311b69fd4060e5830b552376d9d1167e0e6
MysticAakash07 <rahulsagar460@gmail.com>
2025-08-05 15:54:03 +0530

    feat: implement RequireAuth for protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,38 +12,54 @@ import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
 import PersistLogin from "./features/auth/PresistLogin";
+import RequireAuth from "./features/auth/requireAuth";
+import { ROLES } from "./config/roles";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
+				{/* public routes */}
 				<Route index element={<Public />} />
 				<Route path="login" element={<Login />} />
 
+				{/* Protected routes */}
 				<Route element={<PersistLogin />}>
-					<Route element={<Prefetch />}>
-						<Route path="dash" element={<DashLayout />}>
-							<Route index element={<Welcome />} />
+					<Route
+						element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+					>
+						<Route element={<Prefetch />}>
+							<Route path="dash" element={<DashLayout />}>
+								<Route index element={<Welcome />} />
 
-							<Route path="users">
-								<Route index element={<UsersList />} />
-								<Route path=":id" element={<EditUser />} />
-								<Route path="new" element={<NewUserForm />} />
-							</Route>
-							{/* End Users */}
+								<Route
+									element={
+										<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />
+									}
+								>
+									<Route path="users">
+										<Route index element={<UsersList />} />
+										<Route path=":id" element={<EditUser />} />
+										<Route path="new" element={<NewUserForm />} />
+									</Route>
+								</Route>
+								{/* End Users */}
 
-							<Route path="notes">
-								<Route index element={<NotesList />} />
-								<Route path=":id" element={<EditNote />} />
-								<Route path="new" element={<NewNote />} />
+								<Route path="notes">
+									<Route index element={<NotesList />} />
+									<Route path=":id" element={<EditNote />} />
+									<Route path="new" element={<NewNote />} />
+								</Route>
+								{/* End Notes */}
 							</Route>
-							{/* End Notes */}
+							{/* End Dash */}
 						</Route>
-						{/* End Dash */}
+						{/* End Prefetch */}
 					</Route>
-					{/* End Prefetch */}
+					{/* End Require Auth ...ALL */}
 				</Route>
 				{/* End PresistLogin */}
+				{/* End Protected Routes */}
 			</Route>
 		</Routes>
 	);
--- frontend/src/features/notes/EditNoteForm.jsx
+++ frontend/src/features/notes/EditNoteForm.jsx
@@ -3,8 +3,11 @@ import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "../../hooks/useAuth";
 
 const EditNoteForm = ({ note, users }) => {
+	const { isManager, isAdmin } = useAuth();
+
 	const [updateNote, { isLoading, isSuccess, isError, error }] =
 		useUpdateNoteMutation();
 	const [
@@ -38,6 +41,23 @@ const EditNoteForm = ({ note, users }) => {
 		await deleteNote({ id: note._id });
 	};
 
+	const created = new Date(note.createdAt).toLocaleString("en-IN", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+	});
+	const updated = new Date(note.updatedAt).toLocaleString("en-IN", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+	});
+
 	const onTitleChange = (e) => setTitle(e.target.value);
 	const onTextChange = (e) => setText(e.target.value);
 	const onUserIdChange = (e) => setUserId(e.target.value);
@@ -46,6 +66,19 @@ const EditNoteForm = ({ note, users }) => {
 
 	const errClass = isError || isDelError ? "errmsg" : "offscreen";
 	const errContent = error?.data?.message || delerror?.data?.message || "";
+
+	let deleteButton = null;
+	if (isManager || isAdmin) {
+		deleteButton = (
+			<button
+				className="icon-button"
+				title="Delete"
+				onClick={onDeleteNoteClicked}
+			>
+				<FontAwesomeIcon icon={faTrashCan} />
+			</button>
+		);
+	}
 
 	const validTitleClass = !title ? "form__input--incomplete" : "";
 	const validTextClass = !text ? "form__input--incomplete" : "";
@@ -59,10 +92,9 @@ const EditNoteForm = ({ note, users }) => {
 	return (
 		<>
 			<p className={errClass}>{errContent}</p>
-
 			<form className="form" onSubmit={(e) => e.preventDefault()}>
 				<div className="form__title-row">
-					<h2>Edit Note</h2>
+					<h2>Edit Note #{note.ticket}</h2>
 					<div className="form__action-buttons">
 						<button
 							className="icon-button"
@@ -72,13 +104,7 @@ const EditNoteForm = ({ note, users }) => {
 						>
 							<FontAwesomeIcon icon={faSave} />
 						</button>
-						<button
-							className="icon-button"
-							title="Delete"
-							onClick={onDeleteNoteClicked}
-						>
-							<FontAwesomeIcon icon={faTrashCan} />
-						</button>
+						{deleteButton}
 					</div>
 				</div>
 
@@ -105,34 +131,49 @@ const EditNoteForm = ({ note, users }) => {
 					value={text}
 					onChange={onTextChange}
 				/>
+				<div className="form__row">
+					<div className="form__divider">
+						<label
+							className="form__label form__checkbox-container"
+							htmlFor="note-completed"
+						>
+							WORK COMPLETE:
+							<input
+								className="form__checkbox"
+								id="note-completed"
+								name="note-completed"
+								type="checkbox"
+								checked={completed}
+								onChange={(e) => setCompleted(e.target.checked)}
+							/>
+						</label>
 
-				<label
-					className="form__label form__checkbox-container"
-					htmlFor="note-completed"
-				>
-					COMPLETED:
-					<input
-						className="form__checkbox"
-						id="note-completed"
-						name="note-completed"
-						type="checkbox"
-						checked={completed}
-						onChange={(e) => setCompleted(e.target.checked)}
-					/>
-				</label>
-
-				<label className="form__label" htmlFor="userId">
-					ASSIGNED TO:
-				</label>
-				<select
-					id="userId"
-					name="userId"
-					className="form__select"
-					value={userId}
-					onChange={onUserIdChange}
-				>
-					{options}
-				</select>
+						<label className="form__label" htmlFor="userId">
+							ASSIGNED TO:
+						</label>
+						<select
+							id="userId"
+							name="userId"
+							className="form__select"
+							value={userId}
+							onChange={onUserIdChange}
+						>
+							{options}
+						</select>
+					</div>
+					<div className="form__divider">
+						<p className="form__created">
+							Created:
+							<br />
+							{created}
+						</p>
+						<p className="form__updated">
+							Updated:
+							<br />
+							{updated}
+						</p>
+					</div>
+				</div>
 			</form>
 		</>
 	);
